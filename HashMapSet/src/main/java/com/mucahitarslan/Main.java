6bdaf4462785a3df9efcb3b73dc2c3f95eb18217
mucahitarslaan <mucahitarslaan@gmail.com>
2022-10-12 23:55:47 +0300

    updated

---
+++ HashMapSet/src/main/java/com/mucahitarslan/Main.java
@@ -0,0 +1,25 @@
+package com.mucahitarslan;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+        HashMap keyValue = new HashMap();
+        keyValue.put("Türkiye" , "Ankara");
+        keyValue.put("Fransa" , "Paris");
+        keyValue.put("İngiltere", "Londra");
+
+        Set set = keyValue.entrySet();
+        Iterator iterator = set.iterator();
+
+        while (iterator.hasNext())
+        {
+            Map.Entry item = (Map.Entry) iterator.next();
+            System.out.println(item.getKey() + " -> " + item.getValue());
+        }
+    }
+}
