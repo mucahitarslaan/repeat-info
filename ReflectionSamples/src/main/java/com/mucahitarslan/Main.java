6a21f822b143e586aab11439367060215eebf6a5
mucahitarslaan <mucahitarslaan@gmail.com>
2022-12-08 18:37:48 +0300

    update

--- ReflectionSamples/src/main/java/com/mucahitarslan/Main.java
+++ ReflectionSamples/src/main/java/com/mucahitarslan/Main.java
@@ -1,8 +1,38 @@
 package com.mucahitarslan;
+import java.util.Timer;
+import java.util.TimerTask;
 
+class Main {
+    public static void main(String[] args)
+    {
+        /*var timer = new Timer();
 
-public class Main {
-    public static void main(String[] args) {
+        timer.schedule(new TimerTask() {
+            public void run()
+            {
+                System.out.println(".");
+            }
+        }, 0, 1000);
+        //Console.writeLine("main ends");
 
+        //.
+
+         */
+
+        Class<Sample> clsSample = Sample.class;
+        Class<Mample> clsMample = Mample.class;
+        Class<String> clsString = String.class;
+        Class<?> clsDouble = double.class;
+
+        Class<String> clasString1 = String.class;
+        Class<String> clasString2 = String.class;
+
+        System.out.println(clasString1 == clasString2);
     }
+}
+
+class Sample{
+}
+
+class Mample{
 }
